b7bb656143c1f2eb611a4fe3555c15d6aa7ef7df
khushbooraikwar95 <j.khushbooraikwar95@gmail.com>
2026-04-26 07:51:18 +0530

    pdf chunking by langchain textsplitter

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 pdf_path = Path(__file__).parent / "sample_data.pdf"
 loader = PyPDFLoader(pdf_path)
 docs = loader.load()
 # print(docs[3])
 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunks = text_splitter.split_documents(docs)
