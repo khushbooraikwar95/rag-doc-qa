3122c7915e2508999a0d710b64fc0108f8474060
khushbooraikwar95 <j.khushbooraikwar95@gmail.com>
2026-04-26 07:44:28 +0530

    load the pdfs and print the data

--- index.py
+++ index.py
@@ -1,3 +1,8 @@
 from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
 
-pdf_path = Path(__file__).parent / "pdfs"
+pdf_path = Path(__file__).parent / "sample_data.pdf"
+loader = PyPDFLoader(pdf_path)
+docs = loader.load()
+# print(docs[3])
+
